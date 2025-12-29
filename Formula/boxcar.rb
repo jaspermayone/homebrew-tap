c6de4d435586981f776ce6cade44706b058c5984
Jasper Mayone <me@jaspermayone.com>
2025-12-29 14:36:04 -0500

    Bump boxcar to v1.0.1

--- Formula/boxcar.rb
+++ Formula/boxcar.rb
@@ -1,8 +1,8 @@
 class Boxcar < Formula
   desc "Rails app generator with opinionated defaults"
   homepage "https://github.com/jaspermayone/boxcar"
-  url "https://github.com/jaspermayone/boxcar/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "5a799e632270a0707fd72a6c628f4ece3855751ad3db9a0926336220743d9c26"
+  url "https://github.com/jaspermayone/boxcar/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "feaf782a845bd96bbbdaa9f4e62412cb2721def89c55021bb1f6a49383e9000f"
   license "MIT"
 
   def install
