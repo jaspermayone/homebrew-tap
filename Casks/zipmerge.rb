4009a278310d9756dbc095d71836448a9230fc98
Jasper Mayone <mayonej@wit.edu>
2026-02-01 18:44:28 -0500

    Update zipmerge to v2.0.2

--- Casks/zipmerge.rb
+++ Casks/zipmerge.rb
@@ -1,6 +1,6 @@
 cask "zipmerge" do
-  version "2.0.1"
-  sha256 "f1d56db3a88b95cd786a67540c57d2f7288e5d13ef055e48943a3a05c86d7953"
+  version "2.0.2"
+  sha256 "635e79a3f94181d63893b097e80278933133c3fc0232b02f21f4e745cefa13cb"
 
   url "https://github.com/jaspermayone/ZipMerge/releases/download/v#{version}/ZipMerge.zip"
   name "ZipMerge"
